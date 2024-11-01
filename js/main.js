c4de99501fd1e3881a6cc4e16be80f0ef2b2809e
Tiago Marques <tiagomarques2283@hotmail.com>
2024-11-01 16:05:10 -0300

    fazendo a primeira requisição

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("Sucesso");
+
+
+let offset = 0;
+let limit = 10;
+let url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+fetch(url).then( function(response) {
+  console.log(response);
+});
