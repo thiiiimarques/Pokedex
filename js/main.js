eb66f35a4bc50a6fdf0553c5320a14f43b829a6e
Tiago Marques <tiagomarques2283@hotmail.com>
2024-11-06 16:33:44 -0300

    Implementando a pokeapi e construindo uma classe propria de pokemon para facilitar a inserção juntamente com o html

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 function convertPokemonToHtml(pokemon) {
   return `
-        <li class="pokemon">
-        <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
 
         <div class="details">
           <ol class="types">
-            <li class="type">grass</li>
-            <li class="type">poison</li>
+            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
           </ol>
-          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+          <img src="${pokemon.photo}" alt="${pokemon.name}">
         </div>
       </li>
   `
@@ -18,5 +17,6 @@ function convertPokemonToHtml(pokemon) {
 let pokemonList = document.getElementById('pokemonList')
 
   pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('')
-  }) 
+    const newHtml = pokemons.map(convertPokemonToHtml).join('')
+    pokemonList.innerHTML = newHtml
+  })
